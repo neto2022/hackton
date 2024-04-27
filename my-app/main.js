b26a801bd1088926f4da3ad538146bf4999f847e
Neto <97475687+neto2022@users.noreply.github.com>
2024-04-26 22:20:48 -0400

    alguns_testes_com_ol

--- my-app/main.js
+++ my-app/main.js
@@ -2,6 +2,10 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import * as olProj from 'ol/proj';
+
+let a = document.getElementById('ts').getAttribute('value1');
+console.log(`a.value`, a);
 
 const map = new Map({
   target: 'map',
@@ -11,7 +15,7 @@ const map = new Map({
     })
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
+    center: olProj.fromLonLat([-61.1281163,1.813445]),
+    zoom: 20
   })
 });
