7a6b36066e588745c3b28cecf0a249c946e13d24
gelson35 <gelsonbarros6@gmail.com>
2024-04-27 18:25:44 -0400

    modificações na aparencia e no texto

--- my-app/js/main.js
+++ my-app/js/main.js
@@ -1,8 +1,8 @@
-import './../css/style.css';
 import { Map, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
+import OSM from 'ol/source/OSM';
+import './../css/style.css';
 
 //let a = document.getElementById('content').getAttribute('value1');
 //let b = document.getElementById('content').getAttribute('value2');
@@ -28,8 +28,8 @@ const map1 = new Map({
     })
   ],
   view: new View({
-    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
-    zoom: 20
+    center: olProj.fromLonLat([-61.2781724, 3.390454]),
+    zoom: 15
   })
 });
 
@@ -41,8 +41,8 @@ const map2 = new Map({
     })
   ],
   view: new View({
-    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
-    zoom: 20
+    center: olProj.fromLonLat([-60.6878565, 2.838429]),
+    zoom: 12
   })
 });
 const map3 = new Map({
@@ -53,8 +53,8 @@ const map3 = new Map({
     })
   ],
   view: new View({
-    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
-    zoom: 20
+    center: olProj.fromLonLat([-61.1764325, -0.403568]),
+    zoom: 12
   })
 });
 
@@ -66,18 +66,20 @@ const map4 = new Map({
     })
   ],
   view: new View({
-    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
-    zoom: 20
+    center: olProj.fromLonLat([-60.705668, 2.7303008]),
+    zoom: 15
   })
 });
 
 // Pega o modal
 var modal = document.getElementById("myModal");
 var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
 
 // Pega a div que abre o modal
 var div = document.getElementById("myDiv");
 var div2 = document.getElementById("myDiv2");
+var div3 = document.getElementById("myDiv3");
 
 // Quando o usuário clica na div, abre o modal 
 div.addEventListener('click', function () {
@@ -86,6 +88,10 @@ div.addEventListener('click', function () {
 
 div2.addEventListener('click', function () {
   modal2.style.display = "block";
+});
+
+div3.addEventListener('click', function () {
+  modal3.style.display = "block";
 });
 
 // Quando o usuário clica em qualquer lugar fora do modal, fecha ele
@@ -98,6 +104,12 @@ window.addEventListener('click', function (event) {
 window.addEventListener('click', function (event) {
   if (event.target == modal2) {
     modal2.style.display = "none";
+  }
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target == modal3) {
+    modal3.style.display = "none";
   }
 });
 
