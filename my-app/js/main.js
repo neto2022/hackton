c9536b1a4a25beb1163e75b24d4a539468bd6b0a
Neto <97475687+neto2022@users.noreply.github.com>
2024-04-27 05:39:40 -0400

    tela_finalizada

--- my-app/js/main.js
+++ my-app/js/main.js
@@ -4,11 +4,11 @@ import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
 
-let a = document.getElementById('ts').getAttribute('value1');
-let b = document.getElementById('ts').getAttribute('value2');
+//let a = document.getElementById('content').getAttribute('value1');
+//let b = document.getElementById('content').getAttribute('value2');
 
-const map = new Map({
-  target: 'map',
+/*const map = new Map({
+  target: 'map1',
   layers: [
     new TileLayer({
       source: new OSM()
@@ -18,9 +18,47 @@ const map = new Map({
     center: olProj.fromLonLat([a, b]),
     zoom: 20
   })
+});*/
+
+const map1 = new Map({
+  target: 'map1',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
+    zoom: 20
+  })
 });
 
-// Obtenha o elemento pelo ID
+const map2 = new Map({
+  target: 'map2',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
+    zoom: 20
+  })
+});
+const map3 = new Map({
+  target: 'map3',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: olProj.fromLonLat([-61.1281163, -61.1281163]),
+    zoom: 20
+  })
+});
+
+/*// Obtenha o elemento pelo ID
 let elemento = document.getElementById('myDIV');
 
 // Adicione um ouvinte de eventos ao elemento
@@ -31,9 +69,4 @@ elemento.addEventListener('click', function () {
   } else {
     content.style.maxHeight = content.scrollHeight + "px";
   }
-});
-
-
-function myFunction() {
-
-}
+});*/
